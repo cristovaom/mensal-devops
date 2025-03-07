33f7a04ffcd6249a1cac138f5f88810b74533771
Cristovão Martins <cristovaom8855@gmail.com>
2025-03-07 16:48:49 -0300

    cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   const config = new DocumentBuilder()
     .setTitle('Task API')
     .setDescription('The task API description')
